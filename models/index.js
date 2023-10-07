d0bc58bea9ac7e2ef3e3b2090c9375c54e1c9343
Asim Riaz <asim.riaz@gmail.com>
2023-10-07 13:37:16 +0500

    Updates

--- models/index.js
+++ models/index.js
@@ -3,14 +3,16 @@ const { Student } = require("./Student");
 const { Mark } = require("./Mark");
 const { Grade } = require("./Grade");
 const { Head } = require("./Head");
+const { Course } = require("./Course");
 
 (async () => {
-  await mongoose.connect(`mongodb://127.0.0.1:27017/recapsheet`);
+    await mongoose.connect(`mongodb://127.0.0.1:27017/recapsheet`);
 })();
 
 module.exports = {
-  Student,
-  Mark,
-  Grade,
-  Head,
+    Student,
+    Mark,
+    Grade,
+    Head,
+    Course,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,52 @@
-const router = require('express').Router();
-const db = require('../models');
+const router = require("express").Router();
+const db = require("../models");
 
-router.get('/', (req, res)=>{
-    res.render('index.html');
+router.get("/", (req, res) => {
+    res.render("index.html");
 });
 
-router.get('/list', (req, res)=>{
-    db.Course.find().sort({ courseid: 1 })
-    .then(courses => res.render('list.vash', { courses }))  
-})
+router.get("/list", (req, res) => {
+    db.Course.find()
+        .sort({ courseid: 1 })
+        .then((courses) => res.render("list.vash", { courses }));
+});
 
-router.post('/edit', (req, res)=>{
+router.get("/doit", (req, res) => {
+    //db.Course.find().sort({ courseid: 1 }).then((courses) => res.render("list.vash", { courses }));
+    console.log(`Enter Clicked >> ${JSON.stringify(req.query)}`);
+    res.send(`<pre  style="margin: 0;">${req.query.regno}</pre>`);
+});
+
+router.post("/edit", (req, res) => {
     //let courseid = req.body.courseid;
-    db.Course.find({ courseid: Number(req.body.courseid) })
-    .then(course => {
-        console.log(course[0])
-        res.render('edit.vash', { course: course[0] })
-    })  
-})
+    db.Course.find({ courseid: Number(req.body.courseid) }).then((course) => {
+        console.log(course[0]);
+        res.render("edit.vash", { course: course[0] });
+    });
+});
 
-router.get('/save', (req, res)=>{
-    console.log(req.query)
-    res.send(`<pre>${JSON.stringify(req.query)}</pre>`)
+router.post("/save", async (req, res) => {
+    console.log(req.query);
+    let result = await db.Course.updateOne(
+        { courseid: req.body.courseid },
+        {
+            $set: req.body,
+        }
+    );
+    console.log(result);
+
+    if (result.modifiedCount === 1) {
+        res.render("update.vash", { course: { ...req.body } });
+        //res.render("save.vash", { data: JSON.stringify(req.query, null, 4) });
+    }
+    //res.send(`<pre>${JSON.stringify(req.query)}</pre>`);
     //res.end();
     //let courseid = req.body.courseid;
     // db.Course.find({ courseid: Number(req.body.courseid) })
     // .then(course => {
     //     console.log(course[0])
     //     res.render('edit.vash', { course: course[0] })
-    // })  
-})
-
+    // })
+});
 
 module.exports = router;
